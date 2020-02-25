bd475ecbdf7307bb26584bb5d463e5ab36add581
Eric <eric@kumojin.com>
2020-02-24 20:19:18 -0500

    Before mvp

--- src/components/TrmScoringForm.vue
+++ src/components/TrmScoringForm.vue
@@ -1,11 +1,26 @@
 <template>
   <v-card>
     <v-form>
-      <v-text-field label="TR" data-test="tr-input"></v-text-field>
-      <v-text-field label="Awards" data-test="awards-input"></v-text-field>
+      <v-text-field label="TR" data-test="tr-input" outlined></v-text-field>
+      <v-text-field
+        label="Awards"
+        data-test="awards-input"
+        outlined
+      ></v-text-field>
       <v-text-field
         label="Milestones"
         data-test="milestones-input"
+        outlined
+      ></v-text-field>
+      <v-text-field
+        label="Forests"
+        data-test="forests-input"
+        outlined
+      ></v-text-field>
+      <v-text-field
+        label="Cities"
+        data-test="cities-input"
+        outlined
       ></v-text-field>
     </v-form>
   </v-card>
--- tests/e2e/specs/app.spec.js
+++ tests/e2e/specs/app.spec.js
@@ -8,6 +8,7 @@ describe('Terraforming counter app', () => {
       'Terraforming Mars Counter App'
     );
   });
+
   describe('As a player, I can enter my terraform ratings (TR)', () => {
     it('should display an input for TR', () => {
       cy.get('[data-test="tr-input"]').should('be.visible');
@@ -17,6 +18,28 @@ describe('Terraforming counter app', () => {
     });
     it('should display an input for Milestones', () => {
       cy.get('[data-test="milestones-input"]').should('be.visible');
+    });
+    it('should display an input for Forest tiles', () => {
+      cy.get('[data-test="forests-input"]').should('be.visible');
+    });
+    it('should display an input for Cities tiles', () => {
+      cy.get('[data-test="cities-input"]').should('be.visible');
+    });
+  });
+
+  describe('As a player, after entering my score items, I can get my total score', () => {
+    before(() => {
+      cy.get('[data-test="tr-input"]').type(10);
+      cy.get('[data-test="awards-input"]').type(10);
+      cy.get('[data-test="milestones-input"]').type(10);
+      cy.get('[data-test="forests-input"]').type(10);
+      cy.get('[data-test="cities-input"]').type(10);
+    });
+    it('should display an input for Cities tiles', () => {
+      cy.get('[data-test="score-output"]').should('be.visible');
+    });
+    it('should display the sum of all the scoring items', () => {
+      cy.get('[data-test="score-output"]').should('contain', '60');
     });
   });
 });
